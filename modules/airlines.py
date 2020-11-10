969ce344b35460d60def9d0004bcfb94ab58d7b0
ohaidara <ohaidara@letudiant.fr>
2020-11-10 09:35:10 +0100

    changement des identifiants bdd

--- modules/airlines.py
+++ modules/airlines.py
@@ -2,14 +2,15 @@ import mysql.connector as db #import de mysql
 
 #connexion à la bdd
 mydb = mysql.connector.connect(
-  host="91.171.69.177",
-  port="26550",
-  user="oumarh",
-  password="oumarhaidara"
+  host="127.0.0.1",
+  port="30000",
+  user="root",
+  password="root",
+  database="avions"
 )
 
 #nom de variable : mycursor, type : mysql.connector.cursor.MySQLCursor
 mycursor = mydb.cursor()
 
 #Création de la table airlines avec les champs : carrier et name
-#mycursor.execute("CREATE TABLE IF NOT EXISTS airlines (carrier CHAR(2) PRIMARY KEY, name VARCHAR(100))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS airlines (carrier CHAR(2) PRIMARY KEY, name VARCHAR(100))")
