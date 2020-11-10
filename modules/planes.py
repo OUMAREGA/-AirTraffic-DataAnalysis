106428bbff39fca141949ed5be69e73da731d870
OUMAREGA <omarega@allegromusique.fr>
2020-11-10 10:23:19 +0100

    update planes model

--- modules/planes.py
+++ modules/planes.py
@@ -3,23 +3,23 @@ import mysql.connector
 #connect to database
 
 mydb = mysql.connector.connect(
-  host="91.171.69.177",
-  port="26550",
-  user="oumarm",
-  password="oumarmarega",
+  host="127.0.0.1",
+  port="30000",
+  user="root",
+  password="root",
   database="avions"
 )
 
 mycursor = mydb.cursor()
 
 mycursor.execute(
-  "CREATE TABLE planes"
-  "(tailnum INT AUTO_INCREMENT PRIMARY KEY,"
-  "year INT(4),"
-  "yype VARCHAR(50),"
+  "CREATE TABLE planes (tailnum CHAR(6) PRIMARY KEY,"
+  "YEAR YEAR(4),"
+  "type VARCHAR(50),"
   "manufacturer VARCHAR(20),"
   "model VARCHAR(20),"
-  "engines INT(4),"
-  "spped INT(4),"
+  "engines SMALLINT ,"
+  "seats SMALLINT ,"
+  "speed SMALLINT ,"
   "engine VARCHAR(20))"
 )
