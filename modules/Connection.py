16bbb1d2ec791f08661b2fe1c1eba585d5a87801
ii02735 <yadallee.bilaal@outlook.fr>
2020-11-11 14:37:23 +0100

    [Connection] Load CSV data + ENV injection

--- modules/Connection.py
+++ modules/Connection.py
@@ -1,15 +1,23 @@
+import os
 import mysql.connector as db
+from dotenv import load_dotenv
 from os.path import dirname, join
 
-class Connection:
-    def __init__(self):
-        self.host="localhost",
-        self.port="30000",
-        self.user='root',
-        self.password='root',
-        self.database='avions'
-        self.mydb = self.connection()
+racine = dirname(dirname(__file__))
 
+class Connection:
+   
+    def __init__(self):
+
+        load_dotenv(racine+"/.env")
+        
+        self.host = os.environ.get("DB_HOST")
+        self.database= os.environ.get("DB_NAME")
+        self.port = os.environ.get("DB_PORT")
+        self.user = os.environ.get("DB_USER")
+        self.password = os.environ.get("DB_PWD")
+        self.db = self.connection()
+        self.cursor = self.db.cursor()
     
     def connection(self):
         connection = db.connect(
@@ -24,15 +32,23 @@ class Connection:
 
 
     def launch_script(self,script_name:str):
-        mycursor = self.mydb.cursor()
         try:
-            racine = dirname(dirname(__file__))
             chemin_scripts_sql = join(racine, 'sql')
             file = open(chemin_scripts_sql+f"/{script_name}.sql")
-            mycursor.execute(file.read())
-            self.mydb.commit()
+            self.cursor.execute(file.read())
+            self.db.commit()
         except Exception as e:
             raise e
         finally:
             file.close()
+
+    def load_csv_data(self,table_name:str,csv_file:str):
+        self.cursor.execute(f"""
+            LOAD DATA INFILE '/csv_data/{csv_file}'
+            INTO TABLE {table_name}
+            FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+            LINES TERMINATED BY '\n'
+            IGNORE 1 ROWS
+        """)
+        self.db.commit()
 
