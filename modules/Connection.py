9a45cd92af7684e8db401bed1c26a6ba6bbf5878
ii02735 <yadallee.bilaal@outlook.fr>
2020-11-11 14:47:23 +0100

    [MISC] Documentation méthodes Connection

--- modules/Connection.py
+++ modules/Connection.py
@@ -20,6 +20,11 @@ class Connection:
         self.cursor = self.db.cursor()
     
     def connection(self):
+        """
+            Méthode retournant l'objet MySQLConnection
+            afin de pouvoir manipuler la base de données
+            MariaDB/MySQL
+        """
         connection = db.connect(
             host=self.host,
             port=self.port,
@@ -32,6 +37,14 @@ class Connection:
 
 
     def launch_script(self,script_name:str):
+        """
+            Méthode permettant d'exécuter un script SQL
+            depuis le dossier sql
+
+            Parameters
+            ----------
+                script_name (str) : nom du script
+        """
         try:
             chemin_scripts_sql = join(racine, 'sql')
             file = open(chemin_scripts_sql+f"/{script_name}.sql")
@@ -42,7 +55,19 @@ class Connection:
         finally:
             file.close()
 
+    # MÉTHODE TEMPORAIRE AVANT INTERPRÉTATION DES FICHIERS CSV
+    # POUR RESPECTER LA COHÉRENCE DES TYPES DANS LA BASE DE DONNÉES
+
     def load_csv_data(self,table_name:str,csv_file:str):
+        """
+            Méthode permettant de charger des données
+            CSV dans une table
+
+            Parameters
+            ----------
+                table_name (str) : nom de la table qui doit recevoir les données
+                csv_file (str) : nom du fichier CSV
+        """
         self.cursor.execute(f"""
             LOAD DATA INFILE '/csv_data/{csv_file}'
             INTO TABLE {table_name}
