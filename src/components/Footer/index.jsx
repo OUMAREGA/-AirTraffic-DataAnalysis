f7a82255bf8fbb05a7a1423d951c94e529ec3a7b
Ouijdane110 <elidrissi.rioui.ouijdane@gmail.com>
2020-11-13 14:29:24 +0100

    Création page Home

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => <div className="contenair-footer">
+    <h2>Réalisé par :</h2>
+    <div className="personne">
+        <p>Muhammad Bilaal YADALLEE</p> 
+        <p>Oumar MAREGA</p>
+        <p>Oumar HAIDARA</p>
+        <p>Ouijdane EL IDRISSI RIOUI</p>
+    </div>
+</div>
+
+export default Footer;
