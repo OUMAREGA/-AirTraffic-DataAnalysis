650f29d990a7418060a6255dfcf2ec63ed9bf262
ii02735 <yadallee.bilaal@outlook.fr>
2020-11-14 13:57:14 +0100

    [SCRIPT] Script d'importation

--- import.py
+++ import.py
@@ -2,9 +2,14 @@ from modules.Connection import Connection
 
 c = Connection()
 
+c.create('airplanes')
+c.load_csv_data('airplanes','csv_data/airplanes.csv')
 
 c.create("planes")
 c.load_csv_data('flights',"csv_data/planes.csv")
+
+c.create('airports')
+c.load_csv_data('airports',"csv_data/airports.csv")
 
 c.create("flights")
 c.load_csv_data('flights',"csv_data/flights.csv")
